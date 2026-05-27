14d300d97ddf50a6d8a48326c802ef1e5b68387a
SevenGem27 <teo.monf96@gmail.com>
2026-05-27 18:07:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chess-pwa-v3'; // Cambiato il nome per forzare l'aggiornamento dell'app
+const CACHE_NAME = 'chess-pwa-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,29 +6,28 @@ const urlsToCache = [
   './app.js',
   './manifest.json',
   
-  // 1. Librerie Esterne
+  // 1. Librerie
   'https://code.jquery.com/jquery-3.5.1.min.js',
   'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css',
   'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js',
   
-  // 2. Immagini dei Pezzi (Bianchi)
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wP.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wN.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wB.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wR.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wQ.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/wK.png',
+  // 2. Immagini (Bianchi)
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wP.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wN.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wB.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wR.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wQ.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/wK.png',
   
-  // 3. Immagini dei Pezzi (Neri)
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bP.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bN.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bB.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bR.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bQ.png',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/img/chesspieces/wikipedia/bK.png'
+  // 3. Immagini (Neri)
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bP.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bN.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bB.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bR.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bQ.png',
+  'https://chessboardjs.com/img/chesspieces/wikipedia/bK.png'
 ];
 
-// FASE DI INSTALLAZIONE: Scarica tutto in background
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -36,10 +35,9 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
-  self.skipWaiting(); // Forza l'attivazione immediata del nuovo Service Worker
+  self.skipWaiting();
 });
 
-// FASE DI PULIZIA: Rimuove le vecchie versioni della cache
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -54,17 +52,11 @@ self.addEventListener('activate', event => {
   );
 });
 
-// FASE DI RECUPERO DATI (Quando sei Offline)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Se il file/immagine è in cache, usa quello (Offline)
-        if (response) {
-          return response;
-        }
-        // Altrimenti prova a scaricarlo da internet
-        return fetch(event.request);
+        return response || fetch(event.request);
       })
   );
 });
