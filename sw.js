50a88ed954377c36611635f9efb0da80b2200a05
SevenGem27 <teo.monf96@gmail.com>
2026-05-27 21:17:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,15 @@
-const CACHE_NAME = 'chess-pwa-v5';
+const CACHE_NAME = 'chess-pwa-v10';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json',
-  
-  // 1. Librerie
-  'https://code.jquery.com/jquery-3.5.1.min.js',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css',
-  'https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js',
-  
-  // 2. Immagini (Bianchi)
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wP.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wN.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wB.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wR.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wQ.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/wK.png',
-  
-  // 3. Immagini (Neri)
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bP.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bN.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bB.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bR.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bQ.png',
-  'https://chessboardjs.com/img/chesspieces/wikipedia/bK.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
@@ -43,9 +19,7 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
         })
       );
     })
@@ -54,9 +28,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
