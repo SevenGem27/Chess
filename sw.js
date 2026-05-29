2ee93b243a653c9c7714cf340e3ef65f8bfb0f07
SevenGem27 <teo.monf96@gmail.com>
2026-05-29 14:27:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'chess-pwa-v18'; // Versione aggiornata per forzare il ricaricamento
+const CACHE_NAME = 'chess-pwa-v19'; // Versione aggiornata per ripulire la cache
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
-  './stockfish.js' // IL NOSTRO NUOVO MOTORE
+  './stockfish.js'
 ];
 
 self.addEventListener('install', event => {
@@ -28,13 +28,6 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  // --- IL LASCIAPASSARE PER L'INTELLIGENZA ARTIFICIALE ---
-  // Se la richiesta è un upload (POST) o è diretta a Roboflow, il Service Worker si fa da parte
-  if (event.request.method !== 'GET' || event.request.url.includes('roboflow.com')) {
-    return; 
-  }
-
-  // Comportamento standard per tutto il resto dell'app (funzionamento offline)
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
